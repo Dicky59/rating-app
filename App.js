6fa3f794cfa776ff85f42b03f6774619f6fa00a4
Dicky59 <pekkaarvolaine@gmail.com>
2021-01-10 23:33:19 +0200

    Exercise 10.12: environment variables

--- App.js
+++ App.js
@@ -3,17 +3,20 @@ import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/react-hooks';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
-    return (
-      <NativeRouter>
-        <ApolloProvider client={apolloClient}>
-          <Main />
-        </ApolloProvider>
-      </NativeRouter>
-    );
-  };
+  console.log(Constants.manifest);
+
+  return (
+    <NativeRouter>
+      <ApolloProvider client={apolloClient}>
+        <Main />
+      </ApolloProvider>
+    </NativeRouter>
+  );
+};
   
 export default App;
