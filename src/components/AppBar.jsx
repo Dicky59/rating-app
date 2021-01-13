18ca87214f6fbf6d55bb5c5b2efd32b85226868b
Dicky59 <pekkaarvolaine@gmail.com>
2021-01-13 21:25:08 +0200

    Exercise 10.16: sign out

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from 'react';
 import { View, StyleSheet, ScrollView } from "react-native";
-import { Link } from 'react-router-native';
+import { Link, useHistory } from 'react-router-native';
 import Constants from "expo-constants";
 import AppBarTab from './AppBarTab';
 import theme from "../theme";
+import { useQuery, useApolloClient } from "@apollo/react-hooks";
+import { AUTHORIZED_USER } from "../graphql/queries";
+import AuthStorageContext from "../contexts/AuthStorageContext";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,13 +18,45 @@ const styles = StyleSheet.create({
   },
 });
 
-const AppBar = () => (
-  <View style={styles.container}>
-    <ScrollView style={styles.scrollView} horizontal>
-      <Link to='/' component={AppBarTab}>Repositories</Link>
-      <Link to='/signin' component={AppBarTab}>Sign in</Link>
-    </ScrollView>
-  </View>
-);
+const AppBar = () => {
+
+  const authStorage = useContext(AuthStorageContext);
+  const apolloClient = useApolloClient();
+
+  const accessToken = authStorage.getAccessToken();
+
+  let authorizedUser = null;
+
+  if (accessToken) {
+    const { data } = useQuery(AUTHORIZED_USER, {
+      fetchPolicy: 'cache-and-network'
+    });
+
+    if (data) {
+      data.authorizedUser !== null ?
+        authorizedUser = data.authorizedUser
+        :
+        authorizedUser = null;
+    }
+}
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+  };
+  
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView} horizontal>
+        <Link to='/' component={AppBarTab}>Repositories</Link>
+        {authorizedUser ?
+        <Link to='/' onPress={signOut} component={AppBarTab}>Sign Out</Link>
+        :
+        <Link to='/signin' component={AppBarTab}>Sign In</Link>
+        }
+      </ScrollView>
+    </View>
+  );
+};
 
 export default AppBar;
